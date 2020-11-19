d3095f9430de386a393e5b31691407a4aed66a84
amtoaer <amtoaer@outlook.com>
2020-11-19 17:18:38 +0800

    refactor(utils): add hash_str function to just hash string

--- utils/utils.py
+++ utils/utils.py
@@ -7,9 +7,16 @@ class Utils(object):
     工具类，包括哈希函数
     '''
     @staticmethod
-    def hash(block):
+    def hash_object(block) -> str:
         '''
         使用sha256算法得到序列化的区块的哈希
         '''
-        block_str = json.dump(block, sort_keys=True)
+        block_str = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_str).hexdigest()
+
+    @staticmethod
+    def hash_str(string: str) -> str:
+        '''
+        使用sha256算法哈希字符串
+        '''
+        return hashlib.sha256(string.encode()).hexdigest()
