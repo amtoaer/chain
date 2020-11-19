95654f426f7a09226caf5356bbe164b028620f1a
amtoaer <amtoaer@outlook.com>
2020-11-19 17:17:48 +0800

    fix(vdict): previous one has bugs, use a better one

--- tree/vdict.py
+++ tree/vdict.py
@@ -3,8 +3,6 @@ class Vdict(dict):
     键不存在时自动创建子字典的字典类
     '''
 
-    def __getitem__(self, key):
-        # 对于不存在的key,自动在当前位置生成dict
-        if not hasattr(self, key):
-            self[key] = Vdict()
-        return super(Vdict, self).__getitem__(key)
+    def __missing__(self, key):
+        value = self[key] = type(self)()
+        return value
