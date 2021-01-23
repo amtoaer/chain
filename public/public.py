33b9a2c3c297d92abe4f02d8a79a8a09f1310e36
amtoaer <amtoaer@gmail.com>
2021-01-23 21:59:31 +0800

    refactor(public): make code more readable

--- public/public.py
+++ public/public.py
@@ -11,39 +11,40 @@ class Public(object):
         # 索引树
         self.tree = Tree()
 
+    def clear(self):
+        self.__init__()
+
     def new_transaction(self, info):
         '''
         加入新的交易
         '''
-
+        first_time = time.time()
         # 插入区块链
         pos = self.block_chain.append_new_transaction(
             info['hospital'], info['department'], info['doctor'], info['patient'], info['content'])
-
+        second_time = time.time()
         # 插入索引树
         self.tree.update(info['hospital'], info['department'],
                          info['doctor'], info['patient'], pos)
-
-        return self.block_chain.get_latest_transaction()
+        third_time = time.time()
+        # 返回插入链时间和更新索引时间
+        return second_time-first_time, third_time-second_time
 
     def search_transaction(self, info):
 
         # 遍历搜索开始时间
-        time_begin = time.time()
+        first_time = time.time()
 
         self.block_chain.search_transaction(
             info['hospital'], info['department'], info['doctor'], info['patient'])
 
         # 遍历搜索结束
-        time_traversal = time.time()-time_begin
-
-        # 索引树搜索开始时间
-        time_begin = time.time()
+        second_time = time.time()
 
         self.block_chain.get_transaction(self.tree.get(
             info['hospital'], info['department'], info['doctor'], info['patient']))
 
         # 索引树搜索结束
-        time_index = time.time()-time_begin
-
-        return (time_index, time_traversal)
+        third_time = time.time()
+        # 返回遍历搜索所需时间和索引树搜索所需时间
+        return second_time-first_time, third_time-second_time
